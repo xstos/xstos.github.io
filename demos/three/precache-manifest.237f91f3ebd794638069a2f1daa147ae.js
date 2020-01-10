9ee96f0982b62eb914c9c4caf33484f9906aa98c
Chris C <xstos@users.noreply.github.com>
2020-01-09 20:55:14 -0500

    three fiber test

---
+++ demos/three/precache-manifest.237f91f3ebd794638069a2f1daa147ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f51319de24f91b6158d7812173ba08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aa6442f4188b2008273",
+    "url": "/static/css/main.233000fe.chunk.css"
+  },
+  {
+    "revision": "b77cf1a8eb0852225152",
+    "url": "/static/js/2.90f8ce35.chunk.js"
+  },
+  {
+    "revision": "9aa6442f4188b2008273",
+    "url": "/static/js/main.540e8700.chunk.js"
+  },
+  {
+    "revision": "f35b95a1644a9c5ac26e",
+    "url": "/static/js/runtime-main.7c0348bf.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
