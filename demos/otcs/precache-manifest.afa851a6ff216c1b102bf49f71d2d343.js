9eced5d236a187ceee32c7ecb2fdba5bb66519ee
Chris C <xstos@users.noreply.github.com>
2021-03-16 12:37:49 -0400

    add content server token tester

---
+++ demos/otcs/precache-manifest.afa851a6ff216c1b102bf49f71d2d343.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0802e05bc69b0a63d13e7ea20d559ef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91cdbe6df83be5ac5022",
+    "url": "/static/js/2.748bda80.chunk.js"
+  },
+  {
+    "revision": "2c6fb5073cbb72ed245d",
+    "url": "/static/js/main.6c6bf3ca.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
